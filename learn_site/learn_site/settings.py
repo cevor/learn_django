7d303156334bf009997b0ec6c002865999964001
cevor <info.cevor@gmail.com>
2021-10-11 23:27:29 +0300

    format code

--- learn_site/learn_site/settings.py
+++ learn_site/learn_site/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-q2w^bm!-_)ut9lajthn@1gm*17gi6%2+a)jlhjfy4zw=8-t%ms"
-)
+SECRET_KEY = "django-insecure-q2w^bm!-_)ut9lajthn@1gm*17gi6%2+a)jlhjfy4zw=8-t%ms"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
