4b18d542fb1c666fae08a2836b2207b561aa8db4
cevor <info.cevor@gmail.com>
2021-10-15 21:28:19 +0000

    add str function for Page model

--- learn_site/pages/models.py
+++ learn_site/pages/models.py
@@ -7,3 +7,6 @@ class Page(models.Model):
     permalink = models.CharField(max_length=12, unique=True)
     update_date = models.DateTimeField("Last Updated", blank=True)
     bodytext = models.TextField("Page Content", blank=True)
+
+    def __str__(self) -> str:
+        return self.title
